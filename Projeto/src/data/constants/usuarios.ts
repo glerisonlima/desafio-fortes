b7ebd21f0b5f0b538fb67353b7757977e9e701c2
glerisonlima <glerisonlima@gmail.com>
2025-02-24 16:47:00 -0300

    organizando as pastas

---
+++ Projeto/src/data/constants/usuarios.ts
@@ -0,0 +1,8 @@
+import Usuario from "../model/Usuario";
+
+
+const usuarios: Usuario[] = [
+    { id: "b44894b6-31bf-41d5-8359-ba12fb4c56f5	", login: 'Fortes', senha: '123', status: 'ativo', email: 'fortes@example.com', data_cadastro: '01/01/2024' },
+];
+
+export default  usuarios;
